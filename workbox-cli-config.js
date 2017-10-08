c52957df650856f58c613f2271949a138ed31cc1
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2017-10-08 20:30:32 +0530

    Add files via upload

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "H:\\souravinsights\\",
+  "globPatterns": [
+    "**/*.{eot,ttf,woff,otf,woff2,php,json,scss}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "workbox-cli-config.js"
+  ]
+};
